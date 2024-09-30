3abc15f51c4d816c9de9e9064251238864b1b8d1
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-30 17:11:32 +0200

    manca solo la censura alla parola da censurare

---
+++ alternativa.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> alternativa PHP Badwords</title>
+</head>
+<body>
+    <header>
+       <h1>RISPOSTE</h1>  
+    </header>
+
+    <main>
+
+        <div>
+            <div>
+                paragrafo : 
+                 <p>
+                    <?php echo $_GET['paragraf']; ?>
+                 </p>
+            </div>
+           
+            <div>
+                parole da censurare : 
+                <p>
+                    <?php echo $_GET['badword']; ?>
+                </p>
+            </div>
+        </div>
+        <hr>
+        <div>
+            <div>
+                paragrafo : 
+                 <p>
+                    <?php echo $_GET['paragraf']; ?>
+                 </p>
+                 <div>
+                    Lunghezza del paragrafo: <?php echo strlen($_GET['paragraf']); ?>
+                </div>
+            </div>
+           
+            <div>
+                parola censurata : 
+                <p>
+                    <?php echo $_GET['badword']; ?>
+                </p>
+                <div>
+                    Lunghezza parola censurata: <?php echo strlen($_GET['badword']); ?>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+        <hr>
+        <div>
+            <a href="./index.php">
+                torna alla pagina precedente
+            </a>
+        </div>
+        
+    </main>
+    <script>
+        const paragraf = 'bello';
+        const censoredParagraf = paragraf.replaceAll('bello', '*****');
+
+
+    </script>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,38 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+    <header>
+       <h1>PHP Badwords</h1>  
+    </header>
+
+    <main>
+        <form action="./alternativa.php" method="GET">
+            <div>
+                <div>
+                    <label for="paragraf">
+                        paragrafo
+                    </label>
+                </div>
+                <textarea id="paragraf" name="paragraf" placeholder="inserisci il testo"></textarea>
+
+            </div>
+            <div>
+                <div>
+                    <label for="badword">
+                        parole da censurare
+                    </label>
+                </div>
+                <input id="badword" name="badword" type="text" value="" placeholder="inserisci il testo"></input>
+
+            </div>
+            <div>
+                <button>
+                    invia
+                </button>
+            </div>
+
+        </form>
+    </main>
     
 </body>
 </html>
